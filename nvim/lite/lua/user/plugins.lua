1a06144319bdaf873a39fe6ab3778ca73cc22987
James Middleton <james.ben.middleton@gmail.com>
2026-04-04 15:02:55 +0100

    Switch to from packer to vim.pack.

--- nvim/lite/lua/user/plugins.lua
+++ nvim/lite/lua/user/plugins.lua
@@ -1,32 +1,7 @@
-local fn = vim.fn
-
--- Automatically install packer if isn't already
--- stdpath = the default place nvim plugins store their data; ".local/share/nvim"
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1",
-        "https://github.com/wbthomason/packer.nvim", install_path })
-    fn.system({ "git", "-C", install_path, "checkout", "ea0cc3c59f67c440c5ff0bbe4fb9420f4350b9a3" })
-    print("Installing packer close and reopen Neovim...")
-    vim.cmd([[packadd packer.nvim]])
-end
-
-return require("packer").startup(function(use)
-    -- packer will manage itself
-    use { "wbthomason/packer.nvim" }
-
-    -- lua functions required by some plugins
-    use { "nvim-lua/plenary.nvim" }
-
-    -- git integration
-    use { "lewis6991/gitsigns.nvim" }
-
-    -- fuzzy buffer, file, text finder
-    use { "nvim-telescope/telescope.nvim" }
-
-    -- directory tree
-    use { "stevearc/oil.nvim" }
-
-    -- easier lsp language server setup
-    use { "neovim/nvim-lspconfig" }
-end)
+vim.pack.add({
+    "https://github.com/nvim-lua/plenary.nvim", -- lua functions required by some plugins
+    "https://github.com/lewis6991/gitsigns.nvim", -- git integration
+    "https://github.com/nvim-telescope/telescope.nvim", -- fuzzy buffer, file, text finder
+    "https://github.com/stevearc/oil.nvim", -- directory tree
+    "https://github.com/neovim/nvim-lspconfig" -- easier lsp language server setup
+})
